b2483b5879112642719cd01c026a8fda32498968
Subhamkr31 <Subham.ms.31@gmail.com>
2022-06-02 19:26:25 +0530

    up

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,30 +1,26 @@
 const express = require('express');
-const externalModule = require('./logger')
+// const externalModule = require('./logger')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('The constant in logger route has a value '+externalModule.endpoint)
-    console.log('The current batch is '+externalModule.batch)
-    externalModule.log()
-    res.send('My first ever api!')
+// router.get('/test-me', function (req, res) {
+//     console.log('The constant in logger route has a value '+externalModule.endpoint)
+//     console.log('The current batch is '+externalModule.batch)
+//     externalModule.log()
+//     res.send('My first ever api!')
+// });
+
+router.get("/sol1", function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr= [1,2,3,5,6,7]
+    let missingNumber
+
+    ///LOGIC WILL GO HERE 
+    res.send(  { data: "missingNumber"  }  );
 });
 
-router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
-});
 
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
-});
 
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
-
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
 
 module.exports = router;
 // adding this comment for no reason
