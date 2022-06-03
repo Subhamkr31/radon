95dc67431393b5898cbbf4e268720df3c569102d
Subhamkr31 <Subham.ms.31@gmail.com>
2022-06-03 13:33:29 +0530

    update

--- src/routes/route.js
+++ src/routes/route.js
@@ -45,11 +45,13 @@ let players =
 router.post("/players", function (req, res) {
     let newArr = [req.body.name, req.body.dob, req.body.gender,req.body.city,req.body.sports]
 
-    if(players.push(newArr)== req.body.name){
-        res.send({msg : req.body.name + "that already exists in the data"})
+    if(newArr == req.body.name){
+        res.send({msg : req.body.name + "that already exists in the data", status: true})
+    }else{
+
+        res.send({ msg: players, status: true })
     }
 
-    res.send({ msg: players, status: true })
 })
 
 
