31c2abb6228bde578c06948e637df1aa21017cef
Subhamkr31 <Subham.ms.31@gmail.com>
2022-06-10 19:22:19 +0530

    up

--- src/index.js
+++ src/index.js
@@ -8,18 +8,47 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Subham_1234:Subham1@cluster0.fzu3b.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
-app.use (
+app.use(
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        console.log("inside GLOBAL MW");
         next();
-  }
-  );
+    }
+);
+
+
+function getThecurrentTimeString() {
+    return new Date().toString();
+}
+app.use(
+    function (req, res, next) {
+        req.time = getThecurrentTimeString();
+        console.log(req.time);
+        next();
+    }
+)
+app.use(
+    function (req, res, next) {
+        const parseIp = (req) =>
+            req.headers['x-forwarded-for']?.split(',').shift()
+            || req.socket?.remoteAddress
+        console.log(parseIp(req))
+        next();
+    });
+
+app.use(
+    function (req, res, next) {
+
+        console.dir(req.path) // '/new'
+
+        next()
+    })
+
 
 app.use('/', route);
 
